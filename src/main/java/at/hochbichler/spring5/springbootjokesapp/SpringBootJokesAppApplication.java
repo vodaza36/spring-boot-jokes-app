0a059bde27468454bb9775d772d06f9e100d0e1d
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-10-08 09:45:10 +0200

    add spring xml configuration

--- src/main/java/at/hochbichler/spring5/springbootjokesapp/SpringBootJokesAppApplication.java
+++ src/main/java/at/hochbichler/spring5/springbootjokesapp/SpringBootJokesAppApplication.java
@@ -4,8 +4,10 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringBootJokesAppApplication {
 
     public static void main(String[] args) {
